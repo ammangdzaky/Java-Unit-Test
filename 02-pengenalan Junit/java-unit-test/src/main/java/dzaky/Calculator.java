a957e3eb5a6a2dcde0a77f41c3bf6d92fd7ac009
ammangdzaky <abdurrahmandzaky01@gmail.com>
2025-06-18 10:58:09 +0800

    update

--- 02-pengenalan Junit/java-unit-test/src/main/java/dzaky/Calculator.java
+++ 02-pengenalan Junit/java-unit-test/src/main/java/dzaky/Calculator.java
@@ -11,6 +11,10 @@ public class Calculator {
     }
 
     public int division(int a,int b){
-        return a/b;
+        if(b == 0){
+            throw new IllegalArgumentException("Division by zero");
+        } else{
+            return a/b;
+        }
     }
 }
--- 02-pengenalan Junit/java-unit-test/src/test/java/dzaky/CalculatorTest.java
+++ 02-pengenalan Junit/java-unit-test/src/test/java/dzaky/CalculatorTest.java
@@ -1,5 +1,6 @@
 package dzaky;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
@@ -8,16 +9,32 @@ public class CalculatorTest {
 
     @Test
     public void testAddSucces(){
-        var result = kalkulator.add(100,100,20);
+        var result = kalkulator.add(50,50);
+        assertEquals(100, result);
     }
 
+//    @Test
+//    public void addAddFailed(){
+//        // misal saya mau test harusnya hasil yang benar itu 100
+//        var result = kalkulator.add(100,100);
+//        //manual
+////        if(result != 100){
+////            throw new RuntimeException("Hasil salah");
+////        }
+//
+//        assertEquals(100, kalkulator.add(100,20));
+//    }
+
     @Test
-    public void testDivideSucces(){
-        kalkulator.division(10,5);
+    public void testDivideSuccess(){
+        var result = kalkulator.division(10,5);
+        assertEquals(2, result);
     }
 
     @Test
     public void testDivideFailed(){
-        kalkulator.division(10,0);
+        assertThrows(IllegalArgumentException.class, () -> {
+            kalkulator.division(10,0);
+        });
     }
 }
